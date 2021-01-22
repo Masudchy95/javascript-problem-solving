cef3b91585155fa65e90e72c0e8571b11dc8b39b
Masud Chy <masud3e.095@gmail.com>
2021-01-22 19:46:34 +0600

    Third commit

--- assignment.js
+++ assignment.js
@@ -1,21 +1,33 @@
 //  https://github.com/Masudchy95/javascript-problem-solving
 
 // Task-01: Convert the value of kilometer to meter.
+
 function kilometerToMeter(kilometer){
     var meter = (kilometer * 1000);
     return meter;
 }
- var result = kilometerToMeter(40);
- console.log(result);
+//  var result = kilometerToMeter(40);
+//  console.log(result);
 
 
-// Task-02: 
-function budgetCalculator(){
+// Task-02:Find out the total budget for buying different items of different prices.
 
+var watchPrice = 50;
+var mobilePrice = 100;
+var laptopPrice = 500;
+function budgetCalculator(watch, mobile, laptop){
+    var watchCost = (watch * watchPrice);
+    var mobileCost = (mobile * mobilePrice);
+    var laptopCost = (laptop * laptopPrice);
+    var totalCost = watchCost + mobileCost + laptopCost;
+    return totalCost;
 }
+// var result = budgetCalculator(2, 10, 2);
+// console.log(result);
 
 
 // Task-03: Find out the total cost of staying total no.of days at hotel.
+
 function hotelCost(days){
     var cost = 0;       
     if(days <= 10){
@@ -36,14 +48,20 @@ function hotelCost(days){
     }
     return cost;
 }
-var totalCost = hotelCost(70);
-console.log(totalCost);
+// var totalCost = hotelCost(70);
+// console.log(totalCost);
 
 
 // Task-04: Find out the longest word from an array.
-var friends = ["Salam", "Barkat", "Rafique", "Jabbar", "Hamid", "Jahangir"];
-function megaFriend(longestString){
-    var longestString = friends[0];
-}
 
-
+function megaFriend(arrName){
+    var largest= " ";
+    for (i=0; i<arrName.length; i++){
+        if (arrName[i].length > largest.length){
+          largest = arrName[i];
+        }
+    }
+    return largest;
+  }
+// var result = megaFriend(["Salam", "Barakat", "Rafique", "Jabbar", "Hamid", "Jahangir"]);
+// console.log(result);
